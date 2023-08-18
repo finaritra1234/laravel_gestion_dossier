3b5f2c3fa7d27a9b5688127c42e0420308a9ca8e
Heritina Finaritra <rakotonavalonaheritiana@gmail.com>
2023-08-18 13:08:47 +0200

    Modification du dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Traitement;
 use App\Models\Admin;
+use App\Models\Responsable;
 use DB;
 class AdminController extends Controller
 {
@@ -21,6 +22,10 @@ class AdminController extends Controller
       $acceper = Traitement::where('status','ACCEPTER')->get();
       $attente = Traitement::where('status',null)->get();
       $admin = Admin::get();
+      $resp = Responsable::get();
+
+    //   $pas_envoye = Traitement::where('date_envoye','!=', null)->get();
+    //   $count_pas_envoye = count($pas_envoye);
 
       $count_rejeter = count($rejeter);
       $count_accepter = count($acceper);
@@ -34,7 +39,9 @@ class AdminController extends Controller
         'count_attente' => $count_attente,
         'dossiers' => $dossiers,
         'count_admin' => $count_admin,
-        'admins' => $admin
+        'admins' => $admin,
+        'resps' => $resp,
+      //  'pas_envoye' => $pas_envoye
 
       ]);
    }
@@ -47,7 +54,7 @@ class AdminController extends Controller
         ->select('traitements.created_at','traitements.id','traitements.date_depot','traitements.date_envoye','traitements.date_retour','traitements.status','traitements.motif','traitements.enseignant_id','traitements.dossier_id','traitements.responsable_id',
                 'enseignants.im','enseignants.nom as nom_enseignant','enseignants.prenom','enseignants.date_naiss','enseignants.lieu_naiss','enseignants.corps','enseignants.grad','enseignants.indice','enseignants.acte',
                 'dossiers.type_dossier','responsables.nom')
-        ->orderby('created_at','desc')        
+        ->orderby('created_at','desc')
         ->take(5)
         ->get();
    }
